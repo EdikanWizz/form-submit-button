1207f5bb6ab66cdd78ca74259fc5e39278eeb58b
Chukwuma Wisdom <96079652+EdikanWizz@users.noreply.github.com>
2023-03-08 15:48:24 +0400

    Add files via upload

---
+++ form-button.js
@@ -0,0 +1,110 @@
+//var res= str.slice(12,13);
+//.log(res);
+//document.write(res)
+//document.getElementById
+//var res= str.charAt(1);
+
+//var str="apple, banana, kiwi";
+ //const demo=document.querySelector('.demo');
+ //demo.innerHTML= str
+
+//var str="hello, world";
+// const demo= document.querySelector('.demo');
+ //demo.innerHTML=str.charAt(6);
+ //const demo= document.querySelector('.demo');
+ //demo.innerHTML=str.charAt(6);
+
+// const button= document.querySelector('.btn');
+ //button.addEventListener('click', e=>{
+    // e.preventDefault();
+    // const demo= document.querySelector('.demo');
+ //demo.innerHTML=str.charAt(7);
+ //console.log(demo)
+ //}
+ //);*/
+
+
+
+ //ALERT
+/*const button= document.querySelector('.btn');
+const button=addEventListener('.click', e =>{
+function myfunction(){
+    var str="hello world";
+    alert(str.trim());
+}})
+myfunction();*/
+
+//BUTTON
+/*const roll=3;
+const beans= roll;
+const button= document.querySelector('.btn');
+button.addEventListener('click',e=>{
+    e.preventDefault();
+    //console.log(roll);
+    //const roll="emeka, uche, landlord";
+    //document.write(beans);
+    document.write(document.getElementsByClassName(".span")
+    .innerHTML= `<span class="span">${beans}</span>`)
+}
+)*/
+/*const x= 123;
+const y= new Number(123);
+
+const span= document.querySelector('.span');
+span.innerHTML=  Number("10.33") +  
+console.log(y)*/
+/*const x= 123;
+const y= new Number(123);
+
+const span= document.querySelector('.span');
+span.innerHTML= parseInt("10") 
+console.log(y)*/
+ 
+ /*const fruits=["banana", "orange", "apple", "mango",];
+ button= document.querySelector('.button');
+ button=addEventListener('click', e =>{
+     fruits.push("lemon");
+    //document.write(fruits);
+    console.log(fruits)
+ });*/
+ /*const fruits=["banana", "orange", "apple", "mango"];
+ flen= fruits.length;
+  const text=fruits;
+ //const h= 2;
+ //console.log(h)
+
+ for(i=0;i< flen; i++);
+// {console.log(text)}
+  const span=document.getElementsByClassName('.demo');
+  span.innerHTML=document.write(flen)
+
+//console.log(fruits);
+ //console.log(flen)*/
+
+ const fruits=["banana", "orange", "apple", "mango"];
+ //span= document.querySelector(".span");
+ //span.innerHTML= fruits.unshift("apple")
+ //function myfunction(){
+   //  console.log(span)
+ // span= document.querySelector(".demo");
+// }
+
+//const password= document.querySelector('password');
+//const password= document.querySelector('value');
+
+//function p(){
+   // if(value=20){console.log(correct)}
+//    else(console.log('wrong'))
+//}
+
+const form= document.querySelector('.signup-form');
+//const username= document.querySelector('#username');
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    //console.log(username.value)
+    form.username.value()
+})
+const username= 'username'
+
+
+ 
